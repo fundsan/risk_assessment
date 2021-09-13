2bb3d3d6d46023c444afc249f88009239812b252
Jake Fund <fundsan@gmail.com>
2021-09-13 17:25:39 +0000

    added scoring

--- scoring.py
+++ scoring.py
@@ -14,7 +14,7 @@ import json
 with open('config.json','r') as f:
     config = json.load(f) 
 
-dataset_csv_path = os.path.join(config['output_folder_path']) 
+output_model_path = os.path.join(config['output_model_path']) 
 test_data_path = os.path.join(config['test_data_path']) 
 
 
@@ -22,4 +22,21 @@ test_data_path = os.path.join(config['test_data_path'])
 def score_model():
     #this function should take a trained model, load test data, and calculate an F1 score for the model relative to the test data
     #it should write the result to the latestscore.txt file
+    deployedname='trainedmodel.pkl'
+    with open(os.getcwd()+'/'+output_model_path+'/'+deployedname, 'rb') as file:
+        model = pickle.load(file)
+    
+    testdata=pd.read_csv(os.getcwd()+'/'+test_data_path+'/'+'testdata.csv')
 
+    X=testdata[['lastmonth_activity','lastyear_activity','number_of_employees']].values
+    y=testdata['exited'].values.ravel()
+
+
+    predicted=model.predict(X)
+
+    f1score=metrics.f1_score(predicted,y)
+    with open(os.getcwd()+'/'+test_data_path+'/'+'latestscore.txt', 'w') as f:
+       print(f1score)
+       f.write(str(f1score))
+if __name__ == '__main__':
+    score_model()
