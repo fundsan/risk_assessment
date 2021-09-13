e287ec8e5524960ef2698e5aab59cc15cfa2f726
Jake Fund <fundsan@gmail.com>
2021-09-13 16:30:18 +0000

    ingest.py

--- ingestion.py
+++ ingestion.py
@@ -19,19 +19,20 @@ output_folder_path = config['output_folder_path']
 #############Function for data ingestion
 def merge_multiple_dataframe():
     #check for datasets, compile them together, and write to an output file
-    filenames = os.listdir(os.getcwd()+input_folder_path)
+    filenames = os.listdir(os.getcwd()+'/'+input_folder_path)
     df_list = pd.DataFrame(columns=['corporation','lastmonth_activity','lastyear_activity','number_of_employees','exited'])
     for each_filename in filenames:
-        df1 = pd.read_csv(os.getcwd()+input_folder_path+each_filename)
+        df1 = pd.read_csv(os.getcwd()+'/'+input_folder_path+'/'+each_filename)
         df_list=df_list.append(df1)
 
     
     result=df_list.drop_duplicates()
-    result.to_csv(os.getcwd()+output_folder_path+'finaldata.csv', index=False)
+    filename='finaldata.csv'
+    result.to_csv(os.getcwd()+'/'+output_folder_path+'/'+filename, index=False)
     dateTimeObj=datetime.now()
     thetimenow=str(dateTimeObj.year)+ '/'+str(dateTimeObj.month)+ '/'+str(dateTimeObj.day)
-    filename='ingestedfiles.txt'
-    f=open(os.getcwd()+output_folder_path+filename,'w')
+    
+    f=open(os.getcwd()+'/'+output_folder_path+'/'+filename,'w')
     record=output_folder_path+' '+filename+' '+str(len(result.index))+' '+str(thetimenow)+'\n'
     f.write(record)
     f.close()
