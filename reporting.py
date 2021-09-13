09ec4339bc5b665445d250ab1b92e255d155fbda
Jake Fund <fundsan@gmail.com>
2021-09-13 20:08:52 +0000

    added reporting.py

--- reporting.py
+++ reporting.py
@@ -2,11 +2,12 @@ import pickle
 from sklearn.model_selection import train_test_split
 import pandas as pd
 import numpy as np
-from sklearn import metrics
+from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
 import seaborn as sns
 import json
 import os
+from diagnostics import model_predictions
 
 
 
@@ -15,7 +16,8 @@ with open('config.json','r') as f:
     config = json.load(f) 
 
 dataset_csv_path = os.path.join(config['output_folder_path']) 
-
+output_model_path = os.path.join(config['output_model_path']) 
+test_data_path = os.path.join(config['test_data_path']) 
 
 
 
@@ -23,7 +25,23 @@ dataset_csv_path = os.path.join(config['output_folder_path'])
 def score_model():
     #calculate a confusion matrix using the test data and the deployed model
     #write the confusion matrix to the workspace
+    testdata=pd.read_csv(os.getcwd()+'/'+test_data_path+'/'+'testdata.csv')
 
+    X=testdata[['lastmonth_activity','lastyear_activity','number_of_employees']].values
+    y=testdata['exited'].values.ravel()
+    
+    predictions = model_predictions()
+    
+    
+    arr=confusion_matrix(y, predictions)
+    
+    confusion_df = pd.DataFrame(arr)
+
+    htmap = sns.heatmap(confusion_df, annot=True)
+
+    figure = htmap.get_figure()    
+    figure.savefig(os.getcwd()+'/'+output_model_path+'/'+'confusionmatrix.png', dpi=400)
+    
 
 
 
