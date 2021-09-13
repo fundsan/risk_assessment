219790d00dc6c30cfe2af8be0894c4f2450ffcc0
Jake Fund <fundsan@gmail.com>
2021-09-13 16:00:54 +0000

    added to data ingestion.py

--- ingestion.py
+++ ingestion.py
@@ -19,8 +19,14 @@ output_folder_path = config['output_folder_path']
 #############Function for data ingestion
 def merge_multiple_dataframe():
     #check for datasets, compile them together, and write to an output file
+    filenames = os.listdir(os.getcwd()+input_folder_path)
+    df_list = pd.DataFrame(columns=['corporation','lastmonth_activity','lastyear_activity','number_of_employees','exited'])
+    for each_filename in filenames:
+        df1 = pd.read_csv(os.getcwd()+input_folder_path+each_filename)
+        df_list=df_list.append(df1)
 
-
-
+    result=df_list.drop_duplicates()
+    result.to_csv(os.getcwd()+output_folder_path+'finaldata.csv', index=False)
+    
 if __name__ == '__main__':
     merge_multiple_dataframe()
